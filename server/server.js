cb5c4d5a144768f55c71509320faa9ae15aa470c
cam1024 <crd9095@yahoo.com>
2023-03-24 22:55:46 -0700

    server

--- server/server.js
+++ server/server.js
@@ -12,10 +12,22 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/'));
+
+  })
 }
 
 app.use(routes);
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
+}
+
+startApolloServer(typeDefs, resolvers);
